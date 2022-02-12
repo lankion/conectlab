bb72f054c97f4cc35e93ea383ad6cba4cc6e44ca
ronaldgadelha <44881454+ronaldgadelha@users.noreply.github.com>
2022-02-12 10:41:52 -0300

    Update SimulatedSpace.js
    Correção do espaço simulado

--- src/components/simulador/SimulatedSpace.js
+++ src/components/simulador/SimulatedSpace.js
@@ -1,10 +1,5 @@
-import { useEffect } from 'react';
-import draw2d from "draw2d";
 import style from './SimulatedSpace.module.css';
 export default function SimulatedSpace(){
-    useEffect(() => {
-        const canvas = new draw2d.Canvas("canvas");
-   }, []);
     const dragEvents ={
         onDragEnter: (e) =>{
             e.preventDefault();
